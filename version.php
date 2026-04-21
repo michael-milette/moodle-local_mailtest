7ef224d070fb1316b27f5afd8f57495710302e08
Michael Milette <michael.milette@tngconsulting.ca>
2026-04-20 20:03:53 -0400

    Release 3.1.8

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mailtest';  // To check on upgrade, that module sits in correct place.
-$plugin->version   = 2025042700;        // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2026042000;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013040500;        // Requires Moodle version 2.5.
-$plugin->release   = '3.1.7';
+$plugin->release   = '3.1.8';
 $plugin->maturity  = MATURITY_STABLE;
